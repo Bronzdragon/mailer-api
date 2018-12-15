462cbbada4e0c02e8af6df21c1c0746d9252240e
Walter Bormans <bronzdragon@gmail.com>
2018-12-15 18:17:19 +0100

    Adds editing form for changing subscribers

--- client/client.js
+++ client/client.js
@@ -1,10 +1,10 @@
 /*jslint esversion:6, asi: true */
-const request = require('request-promise');
+const request = require('request-promise')
 
 class Client {
 
   constructor(mainElement) {
-    this.account = null;
+    this.account = null
     this.element = mainElement
     this.config = require('./config.json')
     if (!this.config.api_endpoint) { throw new Error("No endpoint defined!") }
@@ -57,7 +57,7 @@ class Client {
       this._submitLoginDetails(document.getElementById('name').value, document.getElementById('email').value)
     }
     document.getElementById('show-token-form').onclick = () => {
-      this._promptToken();
+      this._promptToken()
     }
   }
 
@@ -84,7 +84,7 @@ class Client {
       this._submitTokenDetails(document.getElementById('email').value, document.getElementById('token').value)
     }
     document.getElementById('show-login-form').onclick = () => {
-      this._promptLogin();
+      this._promptLogin()
     }
   }
 
@@ -95,16 +95,16 @@ class Client {
     try {
       accountDetails = await this._getAccountRequest()
     } catch (error) {
-      console.error(error);
-      throw new Error(error);
+      console.error(error)
+      throw new Error(error)
     }
-    this.account = accountDetails;
+    this.account = accountDetails
 
     try {
       mailinglists = await this._getAllMailingLists()
     } catch (error) {
-      console.error(error);
-      throw new Error(error);
+      console.error(error)
+      throw new Error(error)
     }
 
     let mailinglistRows = mailinglists.map(entry => {
@@ -135,27 +135,198 @@ class Client {
   async _showMailingListPage(listId){
     this.element.innerHTML = '<h1>Loading list details...</h1>'
 
-    let mailingList = await this._getMailingList(listId);
-    console.log(mailingList);
-    let isOdd = false;
+    let mailingList = await this._getMailingList(listId)
+    let isOdd = false
     let subscribersHtml = mailingList.subscribers.map(entry => {
       let fieldsHtml = entry.fields.map(field => {
         return `<tr><td>${field.name}</td><td>${field.value}</td></tr>`
-      }).join('\n');
+      }).join('\n')
       isOdd = !isOdd
-      return `<div class="${isOdd?"odd":"even"}"><span class="subscriber-prop">Name:</span> ${entry.name}<br><span class="subscriber-prop">Email:</span> ${entry.email}<br><span class="subscriber-prop">State:</span>${entry.state}Fields: <table>${fieldsHtml}</table></div>`
-    }).join('\n');
+      return `<div id="subscriber-list-${entry.id}" class="${isOdd?"odd":"even"}"><span class="subscriber-prop">ID:</span> ${entry.id}<br><span class="subscriber-prop">Name:</span> ${entry.name}<br><span class="subscriber-prop">Email:</span> ${entry.email}<br><span class="subscriber-prop">State:</span> ${entry.state}<br><span class="subscriber-prop">Fields:</span> <table>${fieldsHtml}</table></div>`
+    }).join('\n')
 
     this.element.innerHTML = `
+    <div id="back-button">↩</div>
     <div class="mailingList-heading">Details for<br>
     <div class="list-name">${mailingList.id}. ${mailingList.name}</div></div>
-    <div class="subscriber-list">${subscribersHtml}</div>
-    `
+    <div class="subscriber-list">${subscribersHtml}</div>`
+
+    document.getElementById("back-button").onclick = () => {
+      this._showFrontPage()
+    }
+
+    for (let subscriber of mailingList.subscribers) {
+      document.getElementById(`subscriber-list-${subscriber.id}`).onclick = () => {this._showSubscriberEditingPage(listId, subscriber.id)}
+    }
+  }
+
+  async _showSubscriberEditingPage(listId, subscriberId){
+    this.element.innerHTML = '<h1>Loading list details...</h1>'
+
+    function createElement(type, text = null, id = null, classList = [], event = null, callback = null) {
+      const element = document.createElement(type)
+      if (text) {
+        element.textContent = text
+      }
+      if (id) {
+        element.id = id
+      }
+      if (classList.lenght > 0) {
+        element.classList.add(...classList)
+      }
+      if (event && callback) { element.addEventListener(event, callback) }
+      return element
+    }
+
+    const subscriber = await this._getSubscriber(listId, subscriberId)
+    const fragment = document.createDocumentFragment()
+
+    fragment.appendChild(createElement('div', '↩', 'back-button', [], 'click', () => {
+      this._showMailingListPage(listId)
+    }))
+    const form = createElement('form')
+    fragment.appendChild(form)
+
+    const nameParagraph = createElement('p', 'name:')
+    nameParagraph.appendChild(createElement('br'))
+    const nameInput = createElement('input')
+    nameInput.type = 'text'; nameInput.value = subscriber.name; nameInput.placeholder="Please enter a name"
+    nameParagraph.appendChild(nameInput)
+
+    form.appendChild(nameParagraph)
+
+    const emailParagraph = createElement('p', 'email:')
+    emailParagraph.appendChild(createElement('br'))
+    const emailInput = createElement('input')
+    emailInput.type = 'text'; emailInput.value = subscriber.email; emailInput.placeholder="Please enter an email address"
+    emailParagraph.appendChild(emailInput)
+
+    form.appendChild(emailParagraph)
+
+    const stateParagraph = createElement('p', 'state:')
+    stateParagraph.appendChild(createElement('br'))
+    const stateSelect = createElement('select')
+    for (let state of ['active', 'unsubscribed', 'junk', 'bounced', 'unconfirmed']) {
+      let option = createElement('option', state)
+      option.value = state
+      stateSelect.appendChild(option)
+    }
+    stateParagraph.appendChild(stateSelect)
+
+    form.appendChild(stateParagraph)
+
+    const fieldList = createElement('div', null, 'field-list')
+    form.appendChild(fieldList)
+
+    const newFieldButton = createElement('button', '+ new field')
+    newFieldButton.addEventListener('click', event => {
+      subscriber.fields.push({name: '', value:''})
+      buildFieldList(subscriber.fields)
+      event.preventDefault()
+    })
+    form.appendChild(newFieldButton)
+
+
+    buildFieldList(subscriber.fields)
+
+    this.element.innerHTML = ''
+    this.element.appendChild(fragment);
+
+    function buildFieldList(fields) {
+      function isDate(value = '') { return /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/.test(value) }
+
+      function getType(value) {
+        switch (typeof value) {
+          case 'boolean':
+            return 'true/false'
+          case 'number':
+            return 'number'
+          case 'string': // falls through intentionally
+          default:
+            return isDate(value) ? 'date' : 'text'
+        }
+      }
+
+      function getValueElement(value) {
+        let element = createElement('input')
+        switch (getType(value)) {
+          case 'text': // falls through intentionally
+            element.type = 'text'; element.value = value; element.placeholder="Please provide a value"
+            break
+          case 'true/false':
+            element.type = 'checkbox'; element.value = value
+            break
+          case 'number':
+            element.type = 'number'; element.value = value
+            break
+          case 'date': // falls through intentionally
+            element.type = 'date'; element.value = value
+            break
+        }
+        return element
+      }
+
+      function getTypeDropdown(selectedType) {
+        let element = createElement('select')
+        for (let type of ['text', 'true/false', 'number', 'date']) {
+          let option = createElement('option', type)
+          option.value = type
+          if (type === selectedType){ option.selected = "selected" }
+          element.appendChild(option)
+        }
+        return element
+      }
+
+      function getFieldParagraph(field, index) {
+        const fieldParagraph = createElement("p", null, `field-p-${index}`)
+        const valueInput = getValueElement(field.value)
+        valueInput.id = `field-value-input-${index}`
+        const nameInput = createElement('input', null, `field-name-${index}`)
+        nameInput.type = "text"; nameInput.value = field.name
+        const typeDropdown = getTypeDropdown(getType(field.value))
+        typeDropdown.id = `field-type-select-${index}`
+
+        fieldParagraph.appendChild(typeDropdown)
+        fieldParagraph.appendChild(nameInput)
+        fieldParagraph.appendChild(valueInput)
+
+        typeDropdown.addEventListener('change', event => {
+          switch (event.target.value) {
+            case 'text':
+              field.value = ''
+              break;
+            case 'true/false':
+              field.value = false
+              break;
+            case 'number':
+              field.value = 0
+              break;
+            case 'date':
+              field.value = new Date().toJSON()
+              break;
+            default:
+          }
+          // console.log(event);
+          fieldParagraph.replaceWith(getFieldParagraph(field, index))
+        })
+
+        return fieldParagraph
+      }
+
+      fieldList.innerHTML = ''
+
+      let index = 0
+      for (let field of fields) {
+        fieldList.appendChild(getFieldParagraph(field, index))
+        index++
+      }
+    }
+
   }
 
   async _submitLoginDetails(name, email){
     let errorBanner = document.getElementById('error-banner')
-    let errorMessage = "";
+    let errorMessage = ""
 
     if (!name) { errorMessage += "Please enter a name.<br>" }
     if (!email) { errorMessage += "Please enter an email address.<br>" }
@@ -175,13 +346,13 @@ class Client {
       errorBanner.innerHTML += result.error
       return
     }
-    this.account = result;
+    this.account = result
     this.config.email = this.account.email
     this.config.secret = this.account.secret
   }
 
   _submitTokenDetails(email, token){
-    console.log("Submitting token and email", {token: token, email: email});
+    console.log("Submitting token and email", {token: token, email: email})
     this.config.email = email
     this.config.secret = token
   }
@@ -241,6 +412,16 @@ class Client {
   _getMailingList(listId){
     return request({
       url: `/mailinglists/${listId}/`,
+      baseUrl: this.config.api_endpoint,
+      headers: {apikey : this.config.secret, Email: this.config.email},
+      method: 'get',
+      json: true
+    })
+  }
+
+  _getSubscriber(listId, subscriberId){
+    return request({
+      url: `/mailinglists/${listId}/subscribers/${subscriberId}`,
       baseUrl: this.config.api_endpoint,
       headers: {apikey : this.config.secret, Email: this.config.email},
       method: 'get',
